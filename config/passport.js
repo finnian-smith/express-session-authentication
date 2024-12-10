c35efe241997d99100d93e6d385ab62433d21ada
finnian-smith <finniansmith01@gmail.com>
2024-12-10 16:16:20 +0000

    Build Passport authentication middleware

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,46 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
-import { pool } from "../db/pool.js";
-import crypto from "crypto";
+import db from "../db/queries.js";
+import { validatePassword } from "../lib/passwordUtils.js";
 
-// TODO: passport.use();
+const verifyCallback = async (username, password, done) => {
+  try {
+    const user = await db.getUserByUsername(username);
+
+    if (!user) {
+      return done(null, false, { message: "Incorrect username or password." });
+    }
+
+    const isValid = validatePassword(password, user.hash, user.salt);
+
+    if (isValid) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    done(error);
+  }
+};
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);
+
+// serialize and deserialize user for session management
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id);
+
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
